bcdb0ef319041b57047fdf96972816ee9e25ca1d
schroeder <schroeder@ant.uni-bremen.de>
2026-05-08 12:20:59 +0200

    added optuna functions for rsma and reduced variants of rsma and sdma

---
+++ src/models/optuna_train_reduced.py
@@ -0,0 +1,67 @@
+
+from pathlib import Path
+from sys import path as sys_path
+project_root_path = Path(Path(__file__).parent, '..', '..')
+sys_path.append(str(project_root_path.resolve()))
+
+import optuna
+import numpy as np
+
+from src.config.config import Config
+from src.models.train_sac_reduced import train_sac_reduced
+
+
+def objective(trial):
+
+    # Set up config
+    config = Config()
+    config.show_plots = False
+    config.verbosity = 0
+
+    # Suggest new values for this trial
+    lr_critic = trial.suggest_float(name='lr_critic', low=-6, high=-4)
+    lr_actor = trial.suggest_float(name='lr_actor', low=-7, high=-4)
+
+    lr_critic = 10**lr_critic
+    lr_actor = 10**lr_actor
+
+    config.config_learner.training_episodes = int(0.2 * config.config_learner.training_episodes)  # % of training budget
+    # config.config_learner.training_episodes = 10
+
+    # Apply values to config
+    config.config_learner.network_args['policy_network_optimizer_args']['learning_rate'] = lr_actor
+    config.config_learner.network_args['value_network_optimizer_args']['learning_rate'] = lr_critic
+
+    # execute trial
+    _, metrics = train_sac_reduced(
+        config=config,
+        optuna_trial=trial,
+    )
+
+    window_length = 10
+    window_low = max(len(metrics['mean_sum_rate_per_episode'])-window_length, 0)
+
+    return np.nanmean(metrics['mean_sum_rate_per_episode'][window_low:])
+
+
+if __name__ == '__main__':
+    # steps = episodes
+    study = optuna.create_study(
+        sampler=optuna.samplers.TPESampler(),  # default: TPESampler
+        direction='maximize',
+        pruner=optuna.pruners.MedianPruner(
+            n_startup_trials=2,  # don't prune until n number of trials
+            n_warmup_steps=2000,  # don't prune until n number of steps within a trial
+            n_min_trials=3,  # minimum number of trials to prune, ensure at least n trials survive
+            interval_steps=10,  # number of steps between pruning checks
+        )
+    )
+
+    study.optimize(
+        func=objective,
+        n_trials=100,
+        gc_after_trial=True,  # garbage collection
+    )
+    print(study.best_trial)
+    print(study.best_trials)
+    # print(study.best_params)
