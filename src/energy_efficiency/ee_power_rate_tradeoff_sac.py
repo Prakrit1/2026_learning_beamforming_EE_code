0e64ade14e515f6c1c9b1fef2c894d5b3d147628
Prakrit1 <prakritparajuli628@gmail.com>
2026-09-15 09:47:01 +0000

    Revert legend unit removal: keep 'W' on figure P=.. labels

--- src/energy_efficiency/ee_power_rate_tradeoff_sac.py
+++ src/energy_efficiency/ee_power_rate_tradeoff_sac.py
@@ -142,9 +142,9 @@ if __name__ == '__main__':
     rm_eval_watt = int(round(p_rm))
     legend_handles = [
         Patch(facecolor='0.75', edgecolor='black',
-              label=rf'EE$^{{{trained_watt}}}$, $P={ee_eval_watt}$'),
+              label=rf'EE$^{{{trained_watt}}}$, $P={ee_eval_watt}$ W'),
         Patch(facecolor='0.75', edgecolor='black', hatch='//',
-              label=rf'RM$^{{{trained_watt}}}$, $P={rm_eval_watt}$'),
+              label=rf'RM$^{{{trained_watt}}}$, $P={rm_eval_watt}$ W'),
     ]
     ax_p.legend(handles=legend_handles, loc='upper center', ncol=2, fontsize=10,
                 frameon=False, columnspacing=1.6, handletextpad=0.5)
